0766745f8e08a5d2a2c0900537d840ab935b1548
Payne <wuzhipeng1289690157@gmail.com>
2022-01-08 14:12:49 +0800

    Add deployment plan and fix format

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"github.com/unrolled/secure"
-	"log"
 	"net/http"
 	"strings"
 	"sync"
 )
 
-var LocalPort=":12080"
-var sslPort = ":12443"
+var (
+	// BasicPort The original port without SSL certificate
+	BasicPort = `:12080`
+	// SSLPort "Secure" port with SSL certificate
+	SSLPort = `:12443`
+	// websocket.Upgrader specifies parameters for upgrading an HTTP connection to a
+	// WebSocket connection.
+	upGrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+)
 
+var hlSyncMap sync.Map
+
+// Clients provides Connect instance for a job
 type Clients struct {
 	clientGroup string
 	clientName  string
@@ -22,81 +33,16 @@ type Clients struct {
 	clientWs *websocket.Conn
 }
 
-var upGrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
+// NewClients initializes a new Clients instance
+func NewClients(clientGroup string, clientName string, clientWs *websocket.Conn) *Clients {
+	return &Clients{
+		clientGroup: clientGroup,
+		clientName:  clientName,
+		clientWs:    clientWs,
+	}
 }
 
-var hlClients sync.Map
-
-func NewClient(group string, name string, ws *websocket.Conn) *Clients {
-	
-	client := &Clients{
-		clientGroup: group,
-		clientName:  name,
-		Data:        make(map[string]chan string, 1),
-		//Action:      make(map[string]string),
-		clientWs: ws,
-	}
-	return client
-
-}
-
-func ws(c *gin.Context) {
-	getGroup, getName := c.Query("group"), c.Query("name")
-	if getGroup == "" || getName == "" {
-		return
-	}
-	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		log.Println("websocket err:", err)
-		return
-	}
-	client := NewClient(getGroup, getName, ws)
-	//message := []byte("hello," + getGroup + "->" + getName)
-	//err = ws.WriteMessage(1, message)
-	hlClients.Store(getGroup+"->"+getName, client)
-	for {
-		
-		_, message, err := ws.ReadMessage()
-		if err != nil {
-			break
-		}
-		msg := string(message)
-		//check:=[]uint8{104,108,65,99,116,105,111,110,58,94,95,94}
-		////fmt.Println(msg,string(check)==msg[:12])
-		//if len(msg)>12{
-		//	if string(check)==msg[:12]{
-		//		action:=msg[12:]
-		//		//client.Action[action]=""
-		//		hlClients.Store(getGroup + "->" + getName, client)
-		//	}
-		//}else{
-		//	fmt.Println(msg)
-		//}
-
-		check := []uint8{104, 108, 94, 95, 94}
-
-		strIndex := strings.Index(msg, string(check))
-		if strIndex >= 1 {
-			action := msg[:strIndex]
-			//fmt.Println(action,"save msg")
-			if client.Data[action] == nil {
-				client.Data[action] = make(chan string, 1)
-
-			}
-			client.Data[action] <- msg[strIndex+5:]
-
-			
-			hlClients.Store(getGroup+"->"+getName, client)
-		} else {
-			fmt.Println(msg)
-		}
-		
-	}
-	hlClients.Delete(getGroup + "->" + getName)
-	defer ws.Close()
-}
-
+// QueryFunc Provides context-sensitive methods
 func QueryFunc(client *Clients, funcName string, param string) {
 	var WriteDate string
 	if param == "" {
@@ -113,51 +59,95 @@ func QueryFunc(client *Clients, funcName string, param string) {
 
 }
 
-func Go(c *gin.Context) {
-	getGroup, getName, getAction, getParam := c.Query("group"), c.Query("name"), c.Query("action"), c.Query("param")
-	if getGroup == "" || getName == "" {
+// ws, provides inject function for a job
+func ws(c *gin.Context) {
+	group, name := c.Query("group"), c.Query("name")
+	if group == "" || name == "" {
+		return
+	}
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		fmt.Println("websocket err:", err)
+		return
+	}
+	client := NewClients(group, name, ws)
+	hlSyncMap.Store(group+"->"+name, client)
+	for {
+
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		msg := string(message)
+
+		check := []uint8{104, 108, 94, 95, 94}
+
+		strIndex := strings.Index(msg, string(check))
+		if strIndex >= 1 {
+			action := msg[:strIndex]
+			//fmt.Println(action,"save msg")
+			if client.Data[action] == nil {
+				client.Data[action] = make(chan string, 1)
+
+			}
+			client.Data[action] <- msg[strIndex+5:]
+
+			hlSyncMap.Store(group+"->"+name, client)
+		} else {
+			fmt.Println(msg)
+		}
+
+	}
+	hlSyncMap.Delete(group + "->" + name)
+	defer ws.Close()
+}
+
+// ResultSet provides get result function for a job
+// you can use it to Remote operation and get results
+func ResultSet(c *gin.Context) {
+	//group, name, action, param := c.Query("group"), c.Query("name"), c.Query("action"), c.Query("param")
+	group := c.Query("group")
+	name := c.Query("name")
+	action := c.Query("action")
+	param := c.Query("param")
+
+	if group == "" || name == "" {
 		c.String(200, "input group and name")
 		return
 	}
-	//fmt.Println(getGroup, getName)
-	fmt.Println(getGroup + "->" + getName)
-	clientName, ok := hlClients.Load(getGroup + "->" + getName)
+	fmt.Println(group + "->" + name)
+	clientName, ok := hlSyncMap.Load(group + "->" + name)
 	fmt.Println(clientName)
 	if ok == false {
 		c.String(200, "注入了ws？没有找到当前组和名字")
 		return
 	}
-	if getAction == "" {
-		c.JSON(200, gin.H{"group": getGroup, "name": getName})
+	if action == "" {
+		c.JSON(200, gin.H{"group": group, "name": name})
 		return
 	}
 
 	value, ko := clientName.(*Clients)
-	if value.Data[getAction] == nil {
-		value.Data[getAction] = make(chan string, 1)
+	if value.Data[action] == nil {
+		value.Data[action] = make(chan string, 1)
 	}
-
-
-
-	QueryFunc(value, getAction, getParam)
-	//time.Sleep(time.Second)
-	//data:=value.Action[getAction]
-
-	data := <-value.Data[getAction]
+	QueryFunc(value, action, param)
+	data := <-value.Data[action]
 
 	if ko {
-		c.JSON(200, gin.H{"status": "200", "group": value.clientGroup, "name": value.clientName, getAction: data})
+		c.JSON(200, gin.H{"status": "200", "group": value.clientGroup, "name": value.clientName, action: data})
 	} else {
 		c.JSON(666, gin.H{"message": "?"})
 	}
 
 }
 
-func getList(c *gin.Context) {
+// ClientConnectionList provides get client connect list for a job
+// you can use it see all Connection
+func ClientConnectionList(c *gin.Context) {
 	resList := "hliang:\r\n"
-	hlClients.Range(func(key, value interface{}) bool {
+	hlSyncMap.Range(func(key, value interface{}) bool {
 		resList += key.(string) + "\r\n\t"
-		
 		return true
 	})
 	c.String(200, resList)
@@ -167,7 +157,7 @@ func TlsHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		secureMiddleware := secure.New(secure.Options{
 			SSLRedirect: true,
-			SSLHost:     sslPort,
+			SSLHost:     SSLPort,
 		})
 		err := secureMiddleware.Process(c.Writer, c.Request)
 		if err != nil {
@@ -177,14 +167,13 @@ func TlsHandler() gin.HandlerFunc {
 		c.Next()
 	}
 }
-
 func main() {
 	r := gin.Default()
-	r.GET("/go", Go)
+	r.GET("/result", ResultSet)
 	r.GET("/ws", ws)
-	r.GET("/list", getList)
+	r.GET("/list", ClientConnectionList)
 	r.Use(TlsHandler())
 	//r.Run(LocalPort)
-	r.RunTLS(sslPort, "zhengshu.pem", "zhengshu.key")
+	r.RunTLS(SSLPort, "zhengshu.pem", "zhengshu.key")
 
 }
